bdc82276da7f6b7a1287663b741f303f38c76ab6
Szilard Nemeth <szilard.nemeth88@gmail.com>
2019-03-06 13:55:43 +0100

    ResultPrinter: Delete example printer code (commented)

--- reviewsync/result_printer.py
+++ reviewsync/result_printer.py
@@ -11,14 +11,3 @@ class ResultPrinter:
 
   def print_table_html(self):
     print(tabulate(self.data, self.headers, tablefmt="html"))
-
-  # def print_examples(self):
-  #   format_list = ['plain', 'simple', 'grid', 'fancy_grid', 'pipe', 'orgtbl', 'jira', 'presto', 'psql', 'rst', 'mediawiki', 'moinmoin', 'youtrack', 'html', 'latex', 'latex_raw', 'latex_booktabs']
-  # 
-  #   # Each element in the table list is a row in the generated table
-  #   table = [["spam",42],["eggs",451],["bacon",0]]
-  #   headers = ["item", "qty"]
-  #   
-  #   for f in format_list:
-  #     print("\nformat: {}\n".format(f))
-  #     print(tabulate(table, headers, tablefmt=f))
