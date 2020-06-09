16d2646890fb49f43268f37206f091535492c524
Szilard Nemeth <szilard.nemeth88@gmail.com>
2020-06-09 08:56:44 +0200

    Rewrite project setup info

--- setup.py
+++ setup.py
@@ -12,14 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='hadoop-reviewsync',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Check applicability of patches of specified jiras on given branches',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Szilard Nemeth',
+    author_email='szilard.nemeth88@gmail.com',
+    url='',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
-
